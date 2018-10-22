2ec3d12818d5f5057f78a64b4637d903aee0b42c
Andy Kwan <andykwan@clustertech.com>
2018-10-23 00:25:47 +0800

    testing commit

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,14 @@
 from nltk.tokenize import TreebankWordTokenizer, ToktokTokenizer
 import string
 from nltk.corpus import stopwords
-import re
 import copy
- 
+import re
+
 class FilteredTokenizer(object):
     # turn a doc into clean tokens
     def __init__(self):
         pass
-        
+
     def punctuation_filter(self, tokens=None, punctuation=None, greedy=False):
         if not tokens:
             return tokens
@@ -22,19 +22,19 @@ class FilteredTokenizer(object):
             for tok in tokens:
                 tok = re.sub(regex, tok, '')
             return tokens
-    
+
     def alphabetic_filter(self, tokens=None):
         if not tokens:
             return tokens
         return [word for word in tokens if word.isalpha()]
-    
+
     def stop_words_filter(self, tokens=None, stop_words=None):
         if not tokens:
             return tokens
         if not stop_words:
             stop_words = set(stopwords.words('english'))
         return [w for w in tokens if not w in stop_words]
-    
+
     def filter_and_tokenize(self, list_text, mode=['punctuation'], tokenizer=None):
         if not isinstance(mode, list):
             raise ValueError("Please input a list for mode!")
@@ -59,7 +59,7 @@ class FilteredTokenizer(object):
             return None
         if not isinstance(list_text, list):
             raise ValueError("Please input a list of string for tokenization!")
-        self.list_text = list_text 
+        self.list_text = list_text
         if not tokenizer:
             self.raw_tokens = [text.split() for text in list_text]
         elif "treebank" in tokenizer.lower():
@@ -68,6 +68,6 @@ class FilteredTokenizer(object):
         elif "toktok" in tokenizer.lower():
             t = ToktokTokenizer()
             self.raw_tokens = [t.tokenize(text) for text in list_text]
-            
+
         if not self.raw_tokens:
             return None
